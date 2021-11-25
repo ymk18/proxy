4516777900d27b800c89a06cbaef54d70970c665
Mark <ymk@qq.com>
2021-11-25 10:08:48 +0800

    port

--- main.go
+++ main.go
@@ -11,5 +11,5 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":80"))
+	e.Logger.Fatal(e.Start(":8080"))
 }
